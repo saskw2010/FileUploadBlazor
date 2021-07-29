33366cf92cb406591e242ebc65617ab9eafefcdf
saskw2010 <saskw2010@gmail.com>
2021-07-29 13:17:57 +0300

    Add project files.

---
+++ FileUploadBlazor/Models/SaveFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploadBlazor.Models
+{
+    public class SaveFile
+    {
+        public List<FileData> Files { get; set; }
+    }
+    public class FileData
+    {
+        public byte[] Data { get; set; }
+        public string FileType { get; set; }
+        public long Size { get; set; }
+    }
+}
